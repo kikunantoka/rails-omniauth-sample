2d4699aa70be22bb0b8ef0bcdc1003663c0bd9f3
kikunantoka <kikunantoka@gmail.com>
2017-05-31 15:53:19 +0900

    Add multiple twitter applications

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :customer_users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get '/customer_users' => 'customer_users#show', as: "customer_user_root" 
+  get '/customer_users' => 'customer_users#show', as: "customer_user_root"
 end
